43871fe522655a40999c2833e0f0ffbac0ceab95
Andreas Neukoetter <andreas@omni-mad.com>
2019-12-25 21:19:26 +0100

    + Started to extract archive into struct.

--- 0002-packer-struct/packer/src/main.rs
+++ 0002-packer-struct/packer/src/main.rs
@@ -62,11 +62,94 @@ impl Entry {
 	}
 }
 
+#[derive(Debug)]
+struct Archive {
+	basepath: String,
+	entries: Vec<Entry>
+}
+
+impl Archive {
+	fn create(basepath:&String) -> Archive {
+		Archive {
+			basepath: basepath.clone(),
+			entries: Vec::new(),
+		}
+	}
+	fn add_entry(&mut self, filename:&String) -> bool {
+		let entry = Entry::create(
+			&self.basepath,
+			&filename,
+		);
+
+		self.entries.push(entry);
+		true
+	}
+
+	fn save(&self, output: &String) -> Result<u32,&'static str> {
+		// write output
+		let output_file = File::create(output);
+		// :TODO: rethink error handling
+		let mut output_file = match output_file {
+			Ok( p ) => p,
+			Err( _e ) => return Err("Error writing file"),
+		};
+
+		// :TODO: add error handling
+		let write_names = false;
+		let mut flags: u8 = 0;
+		if write_names {
+			flags |= 1
+		}
+		let number_of_files: u32 = self.entries.len() as u32;
+
+		output_file.write_all(&[
+			0x4f, 0x4d, 0x41, 0x52, 	// magic header
+			2,							// version
+			flags,						// flags
+			0, 0,						// reserved
+		]);
+		output_file.write_u32::<LittleEndian>( number_of_files ).unwrap();
+
+		// write the directory
+		let mut pos = 0;
+		for entry in &self.entries {
+			// crc, pos, size all as LittleEndian u32
+			output_file.write_u32::<LittleEndian>( entry.crc ).unwrap();
+			output_file.write_u32::<LittleEndian>( pos ).unwrap();
+			output_file.write_u32::<LittleEndian>( entry.size ).unwrap();
+
+			pos += entry.size;
+		}
+
+		// write data to output
+
+		for entry in &self.entries {
+
+			let filename = format!( "{}/{}", self.basepath, entry.filename );
+	//		println!("{:?}", filename );
+			let data_file = File::open(filename);
+			// :TODO: rethink error handling
+			let mut data_file = match data_file {
+				Ok( p ) => p,
+				Err( _e ) => return Err("Error reading data file"),
+			};
+			let mut buffer = Vec::<u8>::new();
+			data_file.read_to_end(&mut buffer);
+			output_file.write_all( &buffer );
+		}
+
+		Ok(number_of_files)
+	} 
+}
+
 fn packer(
 		basepath:&String,
 		paklist:&String,
 		output:&String,
 ) -> Result<u32,&'static str> {
+
+	// create a new archive
+	let mut archive = Archive::create(basepath);
 
 	// iterate over paklist to get list of files needed
 
@@ -80,78 +163,15 @@ fn packer(
 
 	let paklist_bufreader = BufReader::new(paklist_file);
 
-	let mut files = Vec::new();
+	let mut files: Vec<Entry> = Vec::new();
 	for line in paklist_bufreader.lines() {
 		let filename = line.unwrap();
 		println!("{:?}", filename );
-		let entry = Entry::create(
-			&basepath,
-			&filename,
-		);
 
-//		entry.display();
-		files.push(entry);
+		archive.add_entry( &filename );
 	}
 
-	// write output
-	let output_file = File::create(output);
-	// :TODO: rethink error handling
-	let mut output_file = match output_file {
-		Ok( p ) => p,
-		Err( _e ) => return Err("Error writing file"),
-	};
-
-	// write header to output
-//	output_file.write_all(&[0x4f, 0x4d, 0x41, 0x52]); // write magic header
-//	output_file.write_all(&[2]); // version
-
-	// :TODO: add error handling
-	let write_names = false;
-	let mut flags: u8 = 0;
-	if write_names {
-		flags |= 1
-	}
-	let number_of_files: u32 = files.len() as u32;
-
-	output_file.write_all(&[
-		0x4f, 0x4d, 0x41, 0x52, 	// magic header
-		2,							// version
-		flags,						// flags
-		0, 0,						// reserved
-	]);
-	output_file.write_u32::<LittleEndian>( number_of_files ).unwrap();
-
-	// write the directory
-	let mut pos = 0;
-	for entry in &files {
-		// crc, pos, size all as LittleEndian u32
-		output_file.write_u32::<LittleEndian>( entry.crc ).unwrap();
-		output_file.write_u32::<LittleEndian>( pos ).unwrap();
-		output_file.write_u32::<LittleEndian>( entry.size ).unwrap();
-
-		pos += entry.size;
-	}
-
-	// write data to output
-
-	for entry in &files {
-
-		let filename = format!( "{}/{}", basepath, entry.filename );
-//		println!("{:?}", filename );
-		let data_file = File::open(filename);
-		// :TODO: rethink error handling
-		let mut data_file = match data_file {
-			Ok( p ) => p,
-			Err( _e ) => return Err("Error reading data file"),
-		};
-		let mut buffer = Vec::<u8>::new();
-		data_file.read_to_end(&mut buffer);
-		output_file.write_all( &buffer );
-	}
-
-	// :TODO:
-//	Err("not implemented")
-	Ok(0)
+	archive.save( output )
 }
 
 fn main() {
